d3e7707b128d90166476d9b1c67794375577c2dc
David Merillat <david.merillat@gmail.com>
2017-06-15 18:54:54 -0400

    Added matrix for calculating oil processing

--- planner.py
+++ planner.py
@@ -3,6 +3,7 @@
 import json
 import sys
 import math
+import numpy as np
 
 class Item(object):
     def __init__(self,name):
@@ -24,6 +25,9 @@ class Item(object):
         'stone':50,
         'water':2500,
         'crude-oil':2500,
+        'heavy-oil':2500,
+        'light-oil':2500,
+        'petroleum-gas':2500,
         'steam':2500
     }
 
@@ -33,7 +37,7 @@ class Item(object):
         #  Ores are 50, fluids are 2500 (10 barrels)
         if self.name in Item.stack_sizes:
             return Item.stack_sizes[self.name]
-        print( "Don't know stack size for " + item.name )
+        print( "Don't know stack size for " + self.name )
         exit(1)
 
     def __repr__(self):
@@ -75,9 +79,66 @@ class Recipe(object):
             self.results.append( (item,result['amount']) )
             item.made_by.append(self)
         self.productivity_allowed = self.name in Module.productivity_recipes
-
+        self.prepared = False
+        
     all = {}
 
+    def prepare(self):
+        if self.prepared:
+            return
+        self.prepared = True
+        
+        self.machine = Machine.best_for_category( self.category )
+        if self.machine is None:
+            print( "No machine for " + self.category )
+            exit(1)
+
+        prod3_module = Module.all['productivity-module-3']
+        speed3_module = Module.all['speed-module-3']
+
+        self.effects = Effects()
+        if self.productivity_allowed:
+            self.effects.apply_module( prod3_module, self.machine.module_slots )
+        else:
+            self.effects.apply_module( speed3_module, self.machine.module_slots )
+
+        # assume 4 speed beacons per machine:
+        self.effects.apply_module( speed3_module, 4 )
+
+        self.effects.clip_mults()
+
+
+    def compute(self):
+        if( len(self.results) > 1 ):
+            print( "Error, recipes with more than one result should be CompoundRecipes" )
+            exit(1)
+            
+        self.prepare()
+        
+        (result,qty_per_batch) = self.results[0]
+        qty_to_make = result.qty_req + result.qty_dep
+        batches_to_make = qty_to_make / (qty_per_batch * self.effects.prod_mult)
+
+        for (item,qty_input) in self.ingredients:
+            item.qty_dep += qty_input * batches_to_make
+
+        (machines_needed, machines_needed_1x) = self.calc_machines_needed(batches_to_make)
+        self.show(machines_needed, machines_needed_1x)
+        self.machine.number_needed += machines_needed
+
+    def calc_machines_needed(self,batches_to_make):
+        crafting_speed = self.machine.crafting_speed * self.effects.speed_mult
+        machines_needed = math.ceil( batches_to_make * self.work / crafting_speed )
+        machines_needed_1x = batches_to_make * self.work
+        return (machines_needed, machines_needed_1x)
+
+    def show(self,machines_needed,machines_needed_1x):
+        prod_mark = "***" if self.productivity_allowed else "   "
+        machine_name = self.machine.abbreviation
+
+        print( "%s (%8.2f) %3d %s - %s" % (prod_mark, machines_needed_1x, machines_needed,
+                                        machine_name, self.name) )
+    
     @staticmethod
     def load_json( recipes_json ):
         for name in recipes_json:
@@ -87,6 +148,92 @@ class Recipe(object):
 
     def __repr__(self):
         return "Recipe(%s)" % self.name
+
+class CompoundRecipe(Recipe):
+    def __init__(self,name,recipes):
+        self.name = name
+        self.recipes = recipes
+        Recipe.all[name] = self
+        self.inputs = set()
+        self.outputs = set()
+        for recipe in recipes:
+            self.inputs.update( {item for (item,qty) in recipe.ingredients} )
+            self.outputs.update( {item for (item,qty) in recipe.results} )
+        self.inputs -= self.outputs
+        
+        for item in self.inputs:
+            item.used_in.append(self)
+        for item in self.outputs:
+            item.made_by.append(self)
+        
+        self.ingredients = [(item,0) for item in self.inputs]
+        self.results = [(item,0) for item in self.outputs]
+    
+    def compute(self):
+        idx = 0
+        item_map = {}
+        for recipe in self.recipes:
+            recipe.prepare()
+            for (item,qty) in recipe.ingredients:
+                if item.name not in item_map:
+                    item_map[item.name] = idx
+                    idx+=1
+            for (item,qty) in recipe.results:
+                if item.name not in item_map:
+                    item_map[item.name] = idx
+                    idx+=1
+        
+        N = len(self.inputs) + len(self.recipes)
+        if N != idx:
+            print( "Error: CompoundRecipe %s is %sconstrained" % (self.name, "under" if N < idx else "over") )
+            print( idx )
+            print( self.inputs )
+            print( self.recipes )
+            
+            exit(1)
+            
+        # Linear equation is M*x = b
+        #   M is recipes (and pseudo-recipes for inputs)
+        #   x is number of batches of each recipe
+        #   b is desired outputs
+        M = np.matrix(np.zeros((N,N)))
+        b = np.matrix(np.zeros((N,1)))
+
+        idx = 0 # column in M, row is given by item_map
+        results = []
+        # populate M
+        for item in self.inputs:
+            M[item_map[item.name],idx] = 1
+            results.append(item)
+            idx += 1
+            
+        for recipe in self.recipes:
+            for (item,qty) in recipe.ingredients:
+                row = item_map[item.name]
+                M[row,idx] += -qty
+            for (item,qty) in recipe.results:
+                row = item_map[item.name]
+                M[row,idx] += qty * recipe.effects.prod_mult
+            results.append(recipe)
+            idx += 1
+                
+        for item in self.outputs:
+            row = item_map[item.name]
+            b[row,0] = item.qty_req + item.qty_dep
+        
+        x = M**-1 * b
+        
+        for (idx,result) in enumerate(results):
+            if type(result) is Item:
+                result.qty_dep += x[idx]
+            if type(result) is Recipe:
+                batches_to_make = x[idx]
+                    
+                (machines_needed, machines_needed_1x) = result.calc_machines_needed(batches_to_make)
+                result.show(machines_needed, machines_needed_1x)
+                result.machine.number_needed += machines_needed
+                
+
 
 class Machine(object):
     by_crafting_category = {}
@@ -175,32 +322,6 @@ def load_all_json(filename):
     Recipe.load_json( recipes_json ) # Need productivity_recipes form Module
     Machine.load_json( machine_json )
 
-def main(argv):
-    load_all_json("data.json")
-    plan_science()
-
-def plan_science():
-    outputs = [
-        ( 'science-pack-1', 1 ),
-        ( 'science-pack-2', 1 ),
-        ( 'science-pack-3', 1 ),
-        ( 'military-science-pack', 1 ),
-        ( 'production-science-pack', 1 ),
-        ( 'high-tech-science-pack', 1 ),
-        ( 'rocket-part', 0.1 ),
-        ( 'satellite', 0.001 ) ]
-
-    global_scalar = 1.0
-
-    forbidden_recipes = set([
-        'basic-oil-processing',
-        'coal-liquefaction',
-        #'advanced-oil-processing',
-        'heavy-oil-cracking',
-        'light-oil-cracking',
-        'solid-fuel-from-petroleum-gas',
-        'solid-fuel-from-heavy-oil'])
-
     machine_abbreviations = {
     "electric-furnace":     "FURN",
     "chemical-plant":       "CHEM",
@@ -208,9 +329,47 @@ def plan_science():
     "rocket-silo":          "SILO",
     "oil-refinery":         "RFNR",
     }
+    
+    for machine in Machine.all:
+        if machine.name in machine_abbreviations:
+            machine.abbreviation = machine_abbreviations[machine.name]
+            
+    oil_recipes = [Recipe.all[x] for x in ( 'advanced-oil-processing',
+                                            'heavy-oil-cracking',
+                                            'light-oil-cracking' ) ]
+    OilProduction = CompoundRecipe( 'compound-oil-production', oil_recipes )
+
+def main(argv):
+    load_all_json("data.json")
+    plan_science()
+
+def plan_science():
+    scipack1 = ('science-pack-1',1)
+    scipack2 = ('science-pack-2',1)
+    scipack3 = ('science-pack-3',1)
+    milt_scipack = ('military-science-pack',1)
+    prod_scipack = ('production-science-pack',1)
+    tech_scipack = ('high-tech-science-pack',1)
+    space_scipack = ('rocket-part',0.1)
+    satellite = ('satellite',.001)
+
+    outputs=[scipack1,scipack2,scipack3,milt_scipack,prod_scipack,tech_scipack,space_scipack,satellite]
+
+    global_scalar = 1.0/1.2 # For productivity modules in labs
+
+    forbidden_recipes = set([
+        'basic-oil-processing',
+        'coal-liquefaction',
+        'advanced-oil-processing',
+        'heavy-oil-cracking',
+        'light-oil-cracking',
+        'solid-fuel-from-petroleum-gas',
+        'solid-fuel-from-heavy-oil'])
 
 
+    # X is a working list of items we need to make
     X = [ Item.all_items[x[0]] for x in outputs ]
+    # D is the working list of items we have already made
     D = []
 
     recipes_used = set()
@@ -263,49 +422,8 @@ def plan_science():
     for (iname,qty) in outputs:
         Item.all_items[iname].qty_req = qty * global_scalar
 
-    prod3_module = Module.all['productivity-module-3']
-    speed3_module = Module.all['speed-module-3']
-
     for recipe in recipe_order:
-        machine = Machine.best_for_category( recipe.category )
-        if machine is None:
-            print( "No machine for " + recipe.category )
-            exit(1)
-
-        eff = Effects()
-        if recipe.productivity_allowed:
-            eff.apply_module( prod3_module, machine.module_slots )
-        else:
-            eff.apply_module( speed3_module, machine.module_slots )
-
-        # assume 4 speed beacons per machine:
-        eff.apply_module( speed3_module, 4 )
-
-        eff.clip_mults()
-
-        batches_to_make = 0;
-        for (result,qty_per_batch) in recipe.results:
-            qty_to_make = result.qty_req + result.qty_dep
-            batches_to_make = max(batches_to_make, qty_to_make / qty_per_batch )
-        batches_to_make /= eff.prod_mult
-
-
-        crafting_speed = machine.crafting_speed * eff.speed_mult
-        machines_needed = math.ceil( batches_to_make * recipe.work / crafting_speed )
-
-        machine.number_needed += machines_needed
-
-        for (item,qty_input) in recipe.ingredients:
-            item.qty_dep += qty_input * batches_to_make
-
-        ingred_names = [item.name for (item,qty) in recipe.ingredients]
-        prod_mark = "***" if recipe.productivity_allowed else "   "
-
-        machine_name = machine.name
-        machine_name = machine_abbreviations.get(machine_name,machine_name)
-
-        print( "%s %8.2f/s %3d %s - %s" % (prod_mark, qty_to_make, machines_needed,
-                                        machine_name, recipe.name) )
+        recipe.compute()
 
     for item in raw_materials:
         stacks_per_min = 60 * item.qty_dep / item.stack_size()
